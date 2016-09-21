59a03eb3a76b93b84042987a7430243ae6c4e7aa
Vladimir Stovbchatij <unreality90@mail.ru>
2016-09-21 17:39:16 +0300

    Fix dependencies

--- Controllers/AdminController.php
+++ Controllers/AdminController.php
@@ -5,7 +5,7 @@ namespace jarrus90\WebsiteComments\Controllers;
 use Yii;
 use yii\base\Module as BaseModule;
 use jarrus90\WebsiteComments\WebsiteCommentsFinder;
-use jarrus90\Admin\Web\Controllers\AdminCrudAbstract;
+use jarrus90\Core\Web\Controllers\AdminCrudAbstract;
 use yii\filters\AccessControl;
 
 class AdminController extends AdminCrudAbstract {
